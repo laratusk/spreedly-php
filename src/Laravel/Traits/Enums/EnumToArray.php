848c0fb61aa34796468202d3972a1118bb14c51f
Elvin Aghalarov <aghalarovelvin@gmail.com>
2026-03-10 14:02:28 +0400

    feature(enums): fix enums

--- src/Laravel/Traits/Enums/EnumToArray.php
+++ src/Laravel/Traits/Enums/EnumToArray.php
@@ -6,26 +6,34 @@ namespace Laratusk\Spreedly\Laravel\Traits\Enums;
 
 trait EnumToArray
 {
+    /** @return list<string> */
     public static function keys(): array
     {
         return array_column(self::cases(), 'name');
     }
 
+    /** @return list<string|int> */
     public static function values(): array
     {
         return array_column(self::cases(), 'value');
     }
 
+    /** @return array<string|int, string> */
     public static function array(): array
     {
         return array_combine(self::values(), self::keys());
     }
 
+    /** @return array<string, string|int> */
     public static function flip(): array
     {
         return array_combine(self::keys(), self::values());
     }
 
+    /**
+     * @param  string|int  ...$keys
+     * @return array<string|int, string>
+     */
     public static function only(...$keys): array
     {
         $all = static::array();
